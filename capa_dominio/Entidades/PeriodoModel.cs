e17cd54cc7d42c324fe2760c5a899625c62db71e
FreddyCH <fcabezahe@ucvvirtual.edu.pe>
2025-10-19 20:42:40 -0500

    Ajustes en capa_dominio.csproj y nuevas clases modelo
    Se ajustó el formato del `ProjectGuid` en `capa_dominio.csproj` para incluir llaves `{}`.
    Se corrigió el formato de las referencias eliminando redundancias.
    Se añadieron las clases `PeriodoModel` y `ReporteModel` al proyecto.
    
    `PeriodoModel` incluye propiedades relacionadas con períodos y un método para validar IDs.
    `ReporteModel` incluye propiedades para datos del trabajador, jornada laboral, ingresos, descuentos y aportes.
    Se actualizaron las referencias en el archivo del proyecto para reflejar estas nuevas clases.

---
+++ capa_dominio/Entidades/PeriodoModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_dominio.Entidades
+{
+    public class PeriodoModel
+    {
+        public int periodo_id { get; set; }
+        public string periodo_nombre { get; set; }
+        public string TipoPago { get; set; }
+        public DateTime periodo_fecha_inicio { get; set; }
+        public DateTime periodo_fecha_fin { get; set; }
+        public string periodo_estado { get; set; }
+
+        public bool EsPeriodoValido(int periodoID)
+        {
+            return periodoID > 0;
+
+        }
+    }
+}
---
+++ capa_dominio/Entidades/ReporteModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capa_dominio.Entidades
+{
+    public class ReporteModel
+    {
+        // RN-01: Datos Completos del Trabajador
+        public string CodigoTrabajador { get; set; }
+        public string NombreCompleto { get; set; }
+        public string TipoIdentificacion { get; set; }
+        public string NumeroIdentificacion { get; set; }
+        public string TipoTrabajador { get; set; }
+        public string SistemaPension { get; set; }
+        public DateTime FechaInicioContrato { get; set; }
+
+        // RN-08: Jornada Laboral Cumplida
+        public string TipoDeJornadaPactada { get; set; }
+        public decimal HorasTrabajadasEstimadas { get; set; }
+        public decimal HorasExtrasReales { get; set; }
+
+        // RN-02: Ingresos Percibidos (Remuneración Total)
+        public decimal SueldoBasico { get; set; }
+        public decimal AsignacionFamiliar { get; set; }
+        public decimal MontoHorasExtras { get; set; }
+        public decimal MontoBonos { get; set; }
+        public decimal OtrosIngresos { get; set; }
+        public decimal TotalHaberesBruto { get; set; }
+
+        // RN-04: Descuentos Legales y Otros Descuentos
+        public decimal AporteSistemaPension { get; set; }
+        public decimal RetencionImpuestoRenta { get; set; }
+        public decimal DescuentoTardanzas { get; set; }
+        public decimal DescuentoAdelantos { get; set; }
+        public decimal TotalDescuentos { get; set; }
+
+        // RN-05: Aportes Obligatorios del Empleador (EsSalud)
+        public decimal BaseImponibleEsSalud { get; set; }
+        public decimal AporteEsSalud { get; set; }
+        public decimal NetoPagar { get; set; }
+        public string PeriodoNomina { get; set; }
+        public DateTime FechaGeneracion { get; set; }
+
+
+    }
+}
