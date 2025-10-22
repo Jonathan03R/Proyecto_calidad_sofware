309088c8e39286a7d71b5c055bf4c8f64cbd5996
Jonathan Roque Gonzales <jonathanjesusroquegonzales@gmail.com>
2025-10-21 22:38:36 -0500

    Refactorización y limpieza en Adelanto_sueldo.cs
    Se eliminaron importaciones redundantes para optimizar el código.
    Se cambió el espacio de nombres a `capa_persistencia.modulo_principal`.
    Se renombró la clase `Adelantos_Repositorio` a `Adelanto_sueldo`.
    Se reemplazó el método `SafeGetString` por lógica inline con `reader.IsDBNull`.
    Se eliminó el método `SafeGetString` y su documentación.
    Se eliminó una implementación redundante de `ObtenerAdelantosPorTrabajador`.

--- capa_persistencia/modulo_principal/Adelanto_sueldo.cs
+++ capa_persistencia/modulo_principal/Adelanto_sueldo.cs
@@ -1,16 +1,13 @@
 ﻿using capa_dominio;
-using capa_dominio.dto;
 using capa_persistencia.modulo_base;
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
-using capa_dominio;                       // AdelantoSueldo, Trabajador
-using capa_persistencia.modulo_base;     // AccesoSQLServer
-using Microsoft.Data.SqlClient;
+
 
 namespace capa_persistencia.modulo_principal
 {
-    public class Adelantos_Repositorio
+    public class Adelanto_sueldo
     {
         private readonly AccesoSQLServer _accesoSQL = new AccesoSQLServer();
 
@@ -41,8 +38,8 @@ namespace capa_persistencia.modulo_principal
 
                             AdelantoMonto = reader.GetDecimal(reader.GetOrdinal("adelanto_monto")),
                             AdelantoFecha = reader.GetDateTime(reader.GetOrdinal("adelanto_fecha")),
-                            AdelantoMotivo = SafeGetString(reader, "adelanto_motivo"),
-                            AdelantoObservaciones = SafeGetString(reader, "adelanto_observaciones")
+                            AdelantoMotivo = reader.IsDBNull(reader.GetOrdinal("adelanto_motivo")) ? null : reader.GetString(reader.GetOrdinal("adelanto_motivo")),
+                            AdelantoObservaciones = reader.IsDBNull(reader.GetOrdinal("adelanto_observaciones")) ? null : reader.GetString(reader.GetOrdinal("adelanto_observaciones"))
                         };
 
                         lista.Add(adelanto);
@@ -61,62 +58,5 @@ namespace capa_persistencia.modulo_principal
             return lista;
         }
 
-        // -----------------------------
-        // Helpers seguros para NULL
-        // -----------------------------
-        private static string SafeGetString(SqlDataReader r, string col)
-        {
-            int i = r.GetOrdinal(col);
-            return r.IsDBNull(i) ? null : r.GetString(i);
-        }
-
-        /// <summary>
-        /// adelantos de sueldo de un trabajador si es null , no tiene adelanto de sueldo
-        /// </summary>
-        /// <param name="trabajadorId"></param>
-        /// <param name="fechaInicio"></param>
-        /// <param name="fechaFin"></param>
-        /// <returns></returns>
-        /// <exception cref="ExcepcionTrabajador"></exception>
-        public List<AdelantoSueldo> ObtenerAdelantosPorTrabajador(int trabajadorId, DateTime fechaInicio, DateTime fechaFin)
-        {
-            var adelantos = new List<AdelantoSueldo>();
-
-            try
-            {
-                _accesoSQL.AbrirConexion();
-                var comando = _accesoSQL.ObtenerComandoDeProcedimiento("proc_obtener_adelantos_por_trabajador");
-                comando.Parameters.AddWithValue("@trabajador_id", trabajadorId);
-                comando.Parameters.AddWithValue("@fecha_inicio", fechaInicio);
-                comando.Parameters.AddWithValue("@fecha_fin", fechaFin);
-
-                using (var reader = comando.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var adelanto = new AdelantoSueldo
-                        {
-                            AdelantoId = reader.GetInt32(reader.GetOrdinal("adelanto_id")),
-                            AdelantoMonto = reader.GetDecimal(reader.GetOrdinal("adelanto_monto")),
-                            AdelantoFecha = reader.GetDateTime(reader.GetOrdinal("adelanto_fecha")),
-                            AdelantoMotivo = reader.GetString(reader.GetOrdinal("adelanto_motivo")),
-                            AdelantoObservaciones = reader.IsDBNull(reader.GetOrdinal("adelanto_observaciones")) ? null : reader.GetString(reader.GetOrdinal("adelanto_observaciones"))
-                        };
-                        adelantos.Add(adelanto);
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                throw new ExcepcionTrabajador(ExcepcionTrabajador.ERROR_DE_CONSULTA);
-            }
-            finally
-            {
-                _accesoSQL.CerrarConexion();
-            }
-
-            return adelantos;
-        }
-       
     }
 }
