f803956e19550c691128effacbd900f8552fc5c1
FreddyCH <fcabezahe@ucvvirtual.edu.pe>
2025-12-04 21:35:33 -0500

    Corrección de cierre de bloque en función anónima
    Se corrigió el cierre de un bloque de instrucciones en el archivo `contratos.data.js`.
    Se eliminó un cierre de bloque `}` y se reemplazó por un cierre adecuado que incluye un punto y coma `;`
    y el cierre de una función anónima autoejecutable `})();`. Este cambio asegura la correcta estructura del código.

--- capa_presentacion/Scripts/contrato/contratos.data.js
+++ capa_presentacion/Scripts/contrato/contratos.data.js
@@ -74,7 +74,7 @@
                 const filtered = (config.Cache.sin || []).filter(x => utils.coincide(x, qn));
                 globalThis.ContratoRender.renderSin(filtered);
             }
-        }
+        }      
 
     };
 
